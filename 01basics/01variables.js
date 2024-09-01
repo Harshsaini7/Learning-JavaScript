cf1d5bf771c241dbe30c9e4ade4259e6a7c57ef0
Harsh Saini <136721570+Harshsaini7@users.noreply.github.com>
2024-09-01 17:06:50 +0000

    variables

---
+++ 01basics/01variables.js
@@ -0,0 +1,15 @@
+const accountId = 753875;
+let accountEmail = "harshsaini1812@gmail.com";
+var accountPassword = 2345;
+accountCity = "Jaipur";
+let accountName;
+
+// accountId = 2; // not allowed because of const variable
+
+accountEmail = "jdhg12@gmail.com";
+accountPassword = 477643;
+accountCity = "Banglore";
+
+console.log(accountId);
+
+console.table([accountId , accountName , accountEmail , accountPassword , accountCity]);
