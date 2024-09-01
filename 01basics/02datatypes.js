6232e7f108aad81db192d8c5bd78afb5be4785e9
Harsh Saini <136721570+Harshsaini7@users.noreply.github.com>
2024-09-01 17:24:29 +0000

    datatypes

---
+++ 01basics/02datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all JS code into new Version
+
+// alert(3+4); // not applicable here , because we are using node.js not browser
+
+console.log(3+4);
+console.log("Harsh");
+
+let name = "Harsh";
+let age = 22;
+let isLoggedIn = false;
+
+//number
+//bigInt
+//string => ""
+//boolean = true/false
+//null => standlone value
+//undefined
+//symbol => unique
+//objects
+
+console.log(typeof null);
+console.log(typeof age);
+
+
