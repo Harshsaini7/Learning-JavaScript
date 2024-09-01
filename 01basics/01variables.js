72ba189fa3a04d703a1b834d59240189591c3d92
Harsh Saini <136721570+Harshsaini7@users.noreply.github.com>
2024-09-01 18:37:51 +0000

    mycommit

--- 01basics/01variables.js
+++ 01basics/01variables.js
@@ -13,3 +13,24 @@ accountCity = "Banglore";
 console.log(accountId);
 
 console.table([accountId , accountName , accountEmail , accountPassword , accountCity]);
+
+//Important Things about variables
+// In JavaScript , there are mainly 2 types of JavaScript
+//1. Primitive   2. Non Primitive
+
+//1.Primitive => {String , Number , Boolean , Null , Undefined , Symbol , BigInt}
+
+//2. Non Primitive => {Arrays , Functions , Objects}
+
+const ary = ["harsh" , "ravi" , "mukesh"];
+
+let myObj = {
+    myname : "harsh",
+    myAge : 22
+};
+
+let myFun = function(){
+    console.log("Hey I am Function");
+}
+
+myFun();
